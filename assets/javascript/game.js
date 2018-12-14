ce48bb4895fe988989861d72fc466de1b006dd9a
Christopher Sinlasath <csinlasath@live.com>
2018-12-13 23:51:23 -0600

    fixed a few bugs with gameLogic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,11 @@
-/*
-Starts:
-    1.  Choose character (click on picture)
-    2.  Player chooses opponent
-    3.  THAT specific opponent is then moved to the defender area
-    4. Attack Phase
-        a. Enable Attack Button
-        b. Deal damage to defending opponent
-        c.  Opponent loses HP(Displayed on bottom of picture)
-        d.  Opponent will attack player if HP is left
-        e.  This will repeat until one or the other dies
-    5.  Player wins if they beat all enemies(HP falls to 0)
-
-Each Character has
-    1.  Each character has Health, Attack Power, Counter Attack Power
-    2.  Each time player attacks Attack Power increases (+6 per attack)
-    3.  Enemies only has Counter Attack Power and HP
-    4.  Enemies do not increase.
-    5.  No Healing or recovering health.
-    6.  Players should be able to win or lose with any character.  Pick the right enemies
-*/
-
-
-
 $(document).ready(function() {
     var possibleCharacters = ["Jar Jar Binks", "Admiral Ackbar", "Lando Calrissian", "Porkins"];
     var possibleCharImages = ["assets/images/jarjar.jpeg", "assets/images/ackbar.jpg", "assets/images/lando.jpeg",  "assets/images/porkins.jpg"];
     var possibleEnemies = ["Kirk", "Spock", "Scotty"];
     var possibleEnemyImages = ["assets/images/kirk.jpg", "assets/images/spock.jpeg", "assets/images/scotty.jpg"];
+
+    var defeatsToWin = possibleCharacters.length + possibleEnemies;
+    var numberOfVictories = 0;
 
     var chosenCharacter = "";
     var isPlayerSelected = false;
@@ -57,9 +36,16 @@ $(document).ready(function() {
             playerCharButton.attr("counter-attack-value", 6);
             playerCharButton.html("<img src=" + possibleCharImages[i] + " width='100%' height='70%'/>" );
             playerCharButton.prepend($(playerCharButton).attr("name-value"));
-            playerCharButton.append("<br></br>" + $(playerCharButton).attr("health-value"));
             $("#your-character").append(playerCharButton);
          }
+    }
+
+    function addHealthToPlayerButton() {
+        $(".selected-player-char").append("<br></br>" + "<p id='selected-player-health'>" + $(".selected-player-char").attr("health-value") + "</p>");
+    }
+
+    function addHealthToEnemyButton() {
+        $(".selected-enemy-char").append("<br></br>" + "<p id='selected-enemy-health'>" + $(".selected-enemy-char").attr("health-value") + "</p>");
     }
     
     function createEnemyCharacters() {
@@ -69,11 +55,10 @@ $(document).ready(function() {
             enemyCharButton.css("width", "200px");
             enemyCharButton.addClass("enemy-char");
             enemyCharButton.attr("name-value", possibleEnemies[i]);
-            enemyCharButton.attr("health-value", 20);
+            enemyCharButton.attr("health-value", 60);
             enemyCharButton.attr("counter-attack-value", 6);
             enemyCharButton.html("<img src=" + possibleEnemyImages[i] + " width='100%' height='70%'/>" );
             enemyCharButton.prepend($(enemyCharButton).attr("name-value"));
-            enemyCharButton.append("<br></br>" + $(enemyCharButton).attr("health-value"));
             $("#enemy-character").append(enemyCharButton);
          }
     }
@@ -102,10 +87,16 @@ $(document).ready(function() {
         enemyHealth -= playerAttack;
         if (enemyHealth < 1) {
             isEnemyDead = true;
+            playerAttack = parseInt(playerAttack) + 6;
             attackButtonEnabled = false;
             $(".selected-enemy-char").remove();
             isEnemySelected = false;
             alert("You defeated " + enemyName + "!");
+            numberOfVictories++;
+            if (numberOfVictories === defeatsToWin){
+                alert("You saved the Galaxy!");
+                resetGame();
+            }
         }
     }
 
@@ -116,13 +107,9 @@ $(document).ready(function() {
             attackButtonEnabled = true;
             console.log(playerAttack);
         }
-        if (isEnemyDead === true) {
-            attackButtonEnabled = false;
-        }
         if (playerHealth < 1) {
             gameOver();
         }
-
     }
 
     function gameOver() {
@@ -157,18 +144,82 @@ $(document).ready(function() {
         attackButtonEnabled = false;
         resetButtonEnabled = false;
         startGame();
+        gameLogic();
+    }
+
+    function gameLogic() {
+        $(".player-char").on("click", function() {
+            if (isPlayerSelected === false) {
+                $(this).css("background-color", "blue");
+                $(this).css("color", "white");
+                $(this).addClass("selected-player-char");
+                $(this).removeClass("player-char");
+                chosenCharacter = $(this).attr("name-value");
+                isPlayerSelected = true;
+                playerHealth = $(this).attr("health-value");
+                playerAttack = $(this).attr("player-attack");
+                playerAttack = $(this).attr("counter-attack-value")
+                addHealthToPlayerButton();
+                moveOtherCharactersToEnemy();
+                showStatsOnPage();
+            }
+        });
+    
+        $(".enemy-char").on("click", function() {
+            if (isEnemySelected === false) {
+                $(this).css("background-color", "red");
+                $(this).css("color", "white");
+                $(this).addClass("selected-enemy-char");
+                enemyName = $(this).attr("name-value");
+                isEnemySelected = true;
+                addHealthToEnemyButton();
+                attackButtonEnabled = true;
+                enemyHealth = $(this).attr("health-value");
+                enemyCounterAttack = $(this).attr("counter-attack-value");
+                $(".selected-enemy-char").appendTo("#defender-area");
+                showStatsOnPage();
+            }
+            
+        });
+    
+        $("#attack-button").on("click", function (){
+            updateEnemyValues();
+            writeValuesToChar();
+            updatePlayerValues();
+            writeValuesToChar();
+            showStatsOnPage();
+        });
+    
+        $("#reset-button").on("click", function() {
+            resetGame();
+        });
     }
 
     function moveOtherCharactersToEnemy() {
         $(".player-char").appendTo("#enemy-character");
+        $(".player-char").addClass("evil-char");
         $(".player-char").addClass("enemy-char");
         $(".player-char").removeClass("player-char");
+        $(".evil-char").on("click", function() {
+            if (isEnemySelected === false) {
+                $(this).css("background-color", "red");
+                $(this).css("color", "white");
+                $(this).addClass("selected-enemy-char");
+                enemyName = $(this).attr("name-value");
+                isEnemySelected = true;
+                addHealthToEnemyButton();
+                attackButtonEnabled = true;
+                enemyHealth = $(this).attr("health-value");
+                enemyCounterAttack = $(this).attr("counter-attack-value");
+                $(".selected-enemy-char").appendTo("#defender-area");
+                showStatsOnPage();
+            }
+        });
     }
 
     function showStatsOnPage() {
-        $("#your-stats").html("<p>Health: </p>" + playerHealth);
-        $("#your-stats").append("<p>Attack: </p>" + playerAttack);
-        $("#your-stats").append("<p>Enemy Health Remaining: </p>" + enemyHealth);
+        $("#selected-player-health").html("<p>Health: </p>" + playerHealth);
+        $("#selected-enemy-health").html("<p>Health: </p>" + enemyHealth);
     }
 
     startGame();
@@ -184,6 +235,7 @@ $(document).ready(function() {
             playerHealth = $(this).attr("health-value");
             playerAttack = $(this).attr("player-attack");
             playerAttack = $(this).attr("counter-attack-value")
+            addHealthToPlayerButton();
             moveOtherCharactersToEnemy();
             showStatsOnPage();
         }
@@ -196,18 +248,24 @@ $(document).ready(function() {
             $(this).addClass("selected-enemy-char");
             enemyName = $(this).attr("name-value");
             isEnemySelected = true;
+            addHealthToEnemyButton();
+            attackButtonEnabled = true;
             enemyHealth = $(this).attr("health-value");
             enemyCounterAttack = $(this).attr("counter-attack-value");
             $(".selected-enemy-char").appendTo("#defender-area");
             showStatsOnPage();
         }
-        
-    })
+    });
 
     $("#attack-button").on("click", function (){
-        updateEnemyValues();
-        updatePlayerValues();
-        showStatsOnPage();
+        if (attackButtonEnabled === true) {
+                updateEnemyValues();
+                updatePlayerValues();
+                showStatsOnPage();
+        }
+        else {
+            alert("Please choose a enemy to attack!");
+        } 
     });
 
     $("#reset-button").on("click", function() {
